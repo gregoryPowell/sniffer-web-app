2c53191c1de867c58468fe1eab5bf062dfe4a0f1
Austin <austin.a.guevara@gmail.com>
2024-08-10 00:15:06 +0100

    example project

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Raspberry Pi Sniffer",
+  description: "General Purpose Sub GHz Sniffer on a raspberry pi",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
